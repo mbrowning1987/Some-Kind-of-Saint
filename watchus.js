ff0a6a0fb7af3b57898a35a4e8206c43b9ce970e
Marissa Browning <marissabrowning1987@gmail.com>
2018-11-25 15:45:50 -0500

    creating gallery

--- watchus.js
+++ watchus.js
@@ -1,4 +1,36 @@
-<button onclick="one()">1</button>
+
+jQuery(document).ready(function($) {
+ 
+    $('#myCarousel').carousel({
+            interval: 5000
+    });
+
+    //Handles the carousel thumbnails
+    $('[id^=carousel-selector-]').click(function () {
+    var id_selector = $(this).attr("id");
+    try {
+        var id = /-(\d+)$/.exec(id_selector)[1];
+        console.log(id_selector, id);
+        jQuery('#myCarousel').carousel(parseInt(id));
+    } catch (e) {
+        console.log('Regex failed!', e);
+    }
+});
+    // When the carousel slides, auto update the text
+    $('#myCarousel').on('slid.bs.carousel', function (e) {
+             var id = $('.item.active').data('slide-number');
+            $('#carousel-text').html($('#slide-content-'+id).html());
+    });
+});
+
+
+
+
+
+
+
+
+{/* <button onclick="one()">1</button>
 <button onclick="two()">2</button>
 <button onclick="four()">4</button>
 
@@ -29,4 +61,4 @@ function four() {
         elements[i].style.flex = "25%"; 
     }
 }
-</script>
+</script> */}
